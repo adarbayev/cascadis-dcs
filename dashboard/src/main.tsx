35b3f021e4ab0f55e7a732344742ce49f663cb6d
adarbayev <aa.darbayev@gmail.com>
2026-08-13 12:45:38 +0100

    Use Poppins across the dashboard

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@fontsource-variable/ibm-plex-sans";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
 import App from "./App";
